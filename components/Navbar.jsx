a6b71220a16767c8199116a9b980f6f4ef784310
shohan-pherones <shohan.sub.56@gmail.com>
2023-05-11 22:13:19 +0600

    courses card added

---
+++ components/Navbar.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import Button from "./Button";
+
+const Navbar = () => {
+  return (
+    <div className="h-20 bg-black text-gray-400 flex items-center">
+      <div className="wrapper flex justify-between">
+        <Link href="/" className="text-white font-semibold">
+          Cordemy
+        </Link>
+
+        <div className="flex gap-5">
+          <Link href="/" className="hover:text-white transition-color">
+            Home
+          </Link>
+          <Link href="/courses" className="hover:text-white transition-color">
+            Courses
+          </Link>
+          <Link href="/about" className="hover:text-white transition-color">
+            About
+          </Link>
+          <Link href="/contact" className="hover:text-white transition-color">
+            Contact
+          </Link>
+        </div>
+
+        <div>
+          <Button href="/login" placeholder="Sign in" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/courses/index.js
+++ pages/courses/index.js
@@ -1,5 +1,40 @@
-const CoursesPage = () => {
-  return <div>CoursesPage</div>;
+import CourseItem from "@/components/CourseItem";
+import SectionHeader from "@/components/SectionHeader";
+import { getAllCourses } from "@/prisma/courses";
+
+const CoursesPage = ({ courses }) => {
+  return (
+    <div className="wrapper py-10">
+      <SectionHeader
+        span="Courses"
+        h2="Browse all courses"
+        p="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          minima vero officiis deserunt amet tenetur."
+      />
+
+      <div className="mt-10 flex flex-wrap gap-10">
+        {courses.map((course) => (
+          <CourseItem key={course.id} course={course} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CoursesPage;
+
+export const getServerSideProps = async () => {
+  const courses = await getAllCourses();
+
+  const updatedCourses = courses.map((course) => ({
+    ...course,
+    updatedAt: course.updatedAt.toString(),
+    createdAt: course.createdAt.toString(),
+  }));
+
+  return {
+    props: {
+      courses: updatedCourses,
+    },
+  };
+};
