b96a5d2016aadd2ef17a2627a081c6007fc67dc9
shohan-pherones <shohan.sub.56@gmail.com>
2023-05-17 22:44:31 +0600

    checkout form added

--- pages/checkout/[courseId].js
+++ pages/checkout/[courseId].js
@@ -1,5 +1,162 @@
-const Checkout = () => {
-  return <div>Checkout</div>;
+import SectionHeader from "@/components/SectionHeader";
+import { getCourse } from "@/prisma/courses";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+const Checkout = ({ course }) => {
+  const { data: session } = useSession();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+    address: "",
+    courseTitle: course.title,
+    price: course.price,
+  });
+
+  useEffect(() => {
+    if (session) {
+      setFormData((prev) => ({
+        ...prev,
+        name: session.user.name,
+        email: session.user.email,
+      }));
+    }
+  }, [session]);
+
+  const handleCheckout = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="wrapper py-10 min-h-screen">
+      <SectionHeader
+        span={"Checkout"}
+        h2={"Please provide your details"}
+        p={"Fill out this form to continue checkout"}
+      />
+
+      <div className="flex justify-center">
+        <form
+          onSubmit={handleCheckout}
+          className="flex flex-col gap-5 mt-10 w-full lg:w-[35rem]"
+        >
+          <div className="form-control flex flex-col gap-2">
+            <label htmlFor="name" className="cursor-pointer">
+              Name
+            </label>
+            <input
+              className="outline-none border py-3 px-4 rounded-lg focus:border-gray-700"
+              type="text"
+              id="name"
+              placeholder="Sarah"
+              value={formData.name}
+              readOnly
+            />
+          </div>
+
+          <div className="form-control flex flex-col gap-2">
+            <label htmlFor="email" className="cursor-pointer">
+              Email
+            </label>
+            <input
+              className="outline-none border py-3 px-4 rounded-lg focus:border-gray-700"
+              type="email"
+              id="email"
+              placeholder="hello@example.com"
+              value={formData.email}
+              readOnly
+            />
+          </div>
+
+          <div className="form-control flex flex-col gap-2">
+            <label htmlFor="mobile" className="cursor-pointer">
+              Phone number
+            </label>
+            <input
+              className="outline-none border py-3 px-4 rounded-lg focus:border-gray-700"
+              type="tel"
+              id="mobile"
+              placeholder="+88017xxxxxxx"
+              value={formData.mobile}
+              onChange={(e) =>
+                setFormData({ ...formData, mobile: e.target.value })
+              }
+            />
+          </div>
+
+          <div className="form-control flex flex-col gap-2">
+            <label htmlFor="address" className="cursor-pointer">
+              Address
+            </label>
+            <input
+              className="outline-none border py-3 px-4 rounded-lg focus:border-gray-700"
+              type="text"
+              id="address"
+              placeholder="ABC Street, NY"
+              value={formData.address}
+              onChange={(e) =>
+                setFormData({ ...formData, address: e.target.value })
+              }
+            />
+          </div>
+
+          <div className="form-control flex flex-col gap-2">
+            <label htmlFor="courseTitle" className="cursor-pointer">
+              Course title
+            </label>
+            <input
+              className="outline-none border py-3 px-4 rounded-lg focus:border-gray-700"
+              type="text"
+              id="courseTitle"
+              placeholder="Advanced JavaScript Course 2023"
+              value={formData.courseTitle}
+              readOnly
+            />
+          </div>
+
+          <div className="form-control flex flex-col gap-2">
+            <label htmlFor="price" className="cursor-pointer">
+              Price (USD)
+            </label>
+            <input
+              className="outline-none border py-3 px-4 rounded-lg focus:border-gray-700"
+              type="number"
+              id="price"
+              placeholder="$100"
+              value={formData.price}
+              readOnly
+            />
+          </div>
+
+          <button
+            role="link"
+            type="submit"
+            className="bg-black py-4 rounded-lg text-white hover:bg-gray-700 duration-300 uppercase"
+          >
+            Proceed to checkout
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Checkout;
+
+export const getServerSideProps = async ({ query }) => {
+  const course = await getCourse(query.courseId);
+
+  const updatedCourse = {
+    ...course,
+    updatedAt: course.updatedAt.toString(),
+    createdAt: course.createdAt.toString(),
+  };
+
+  return {
+    props: {
+      course: updatedCourse,
+    },
+  };
+};
